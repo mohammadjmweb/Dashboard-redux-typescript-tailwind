5d8ced17ae4748377daf5935a87e3cceafee7755
mohammadjmweb <mohammadjmweb@gmail.com>
2025-02-02 07:19:48 +0330

    create main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
-import { QueryClient,QueryClientProvider} from 'react-query' 
+import { BrowserRouter as Router } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.tsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 const queryClient=new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>,
+    <Provider store={store}>
+        <Router>
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
+        </Router>
+    </Provider>
 )
